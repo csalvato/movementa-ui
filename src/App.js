ea1edde7dbb172efe619f7c00ced4f00dafeca21
Chris Salvato <csalvato@gmail.com>
2017-03-06 01:26:09 -0500

    Change to autocomplete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.png';
 import Button from 'react-toolbox/lib/button/Button';
-import Input from 'react-toolbox/lib/input/Input';
+//import Input from 'react-toolbox/lib/input/Input';
+import Autocomplete from 'react-toolbox/lib/autocomplete/Autocomplete';
 import Layout from 'react-toolbox/lib/layout/Layout';
 
-class App extends Component {
-  state = { name: '', hint: '' };
+const source = {
+  'ES-es': 'Spain',
+  'TH-th': 'Thailand',
+  'EN-gb': 'England',
+  'EN-en': 'USA'
+};
 
-  handleChange = (name, value) => {
-    this.setState({...this.state, [name]: value});
-  };
+class App extends Component {
+  state = {
+      simple: ''
+    }
+
+    handleChange = (value) => {
+      this.setState({simple: value});
+    };
 
   render() {
     return (
@@ -19,12 +29,21 @@ class App extends Component {
           <span>MOVEMENTA</span>
           <h1>Learn Gymnastics</h1>
           <h2>Find a gym that allows adults to train.</h2>
-          <Input type='text'
+          {/* <Input type='text'
                  hint='Where do you want to train?'
                  name='name'
                  value={this.state.label}
                  onChange={this.handleChange.bind(this, 'label')}
-                 maxLength={16} />
+                 maxLength={16} /> */}
+          <Autocomplete
+             direction="down"
+             selectedPosition="above"
+             hint="Where do you want to train?"
+             multiple={false}
+             onChange={this.handleChange}
+             source={source}
+             value={this.state.simple}
+           />
           <Button raised primary>FIND A GYM</Button>
         </Layout>
       </div>
