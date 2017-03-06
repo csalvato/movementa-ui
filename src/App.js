53f25de9acdec2c073cbe9b5c20a1f1752ee540f
Chris Salvato <csalvato@gmail.com>
2017-03-06 00:57:39 -0500

    Add home page layout

--- src/App.js
+++ src/App.js
@@ -5,16 +5,28 @@ import Input from 'react-toolbox/lib/input/Input';
 import Layout from 'react-toolbox/lib/layout/Layout';
 
 class App extends Component {
+  state = { name: '', hint: '' };
+
+  handleChange = (name, value) => {
+    this.setState({...this.state, [name]: value});
+  };
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Layout>
+          <img src={logo} className="movementa-logo" alt="logo" />
+          <span>MOVEMENTA</span>
+          <h1>Learn Gymnastics</h1>
+          <h2>Find a gym that allows adults to train.</h2>
+          <Input type='text'
+                 hint='Where do you want to train?'
+                 name='name'
+                 value={this.state.label}
+                 onChange={this.handleChange.bind(this, 'label')}
+                 maxLength={16} />
+          <Button raised primary>FIND A GYM</Button>
+        </Layout>
       </div>
     );
   }
