52f4f764d4006bc4211ac689e4f77993fad452b2
Chris Salvato <csalvato@gmail.com>
2017-03-11 23:29:31 -0500

    Remove copy artifacts

--- src/components/pages/SearchResultsPage/SearchResultsPage.js
+++ src/components/pages/SearchResultsPage/SearchResultsPage.js
@@ -1,8 +1,4 @@
 import React from 'react';
-import logo from './logo.png';
-import Button from 'react-toolbox/lib/button/Button';
-import Autocomplete from 'react-toolbox/lib/autocomplete/Autocomplete';
-import Layout from 'react-toolbox/lib/layout/Layout';
 
 class SearchResultsPage extends React.Component {
   render() {
