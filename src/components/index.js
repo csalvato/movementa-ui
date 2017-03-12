226c7a4dd4dfc63c1f0523edba56fe3d47cc2bd3
Chris Salvato <csalvato@gmail.com>
2017-03-11 22:47:46 -0500

    Add clarifying comments

--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,8 @@ const req = require.context('.', true, /\.\/[^/]+\/[^/]+\/((?!test|stories).)*\.
 
 req.keys().forEach((key) => {
   // Export component names based on the directory in which they live.
+  // For example, "HomePage" component in `./pages/HomePage/index.js` or
+  // `./pages/HomePage/HomePage.js` would be importable as `HomePage`
   const componentName = key.replace(/^.+\/([^/]+)\/([^\.]+)\.js/, '$1')
   module.exports[componentName] = req(key).default
 })
