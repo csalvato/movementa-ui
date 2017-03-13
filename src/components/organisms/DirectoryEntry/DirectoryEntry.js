d296409f0830eb0ff606fdc9d9fea7345eca37a9
Chris Salvato <csalvato@gmail.com>
2017-03-12 23:54:00 -0400

    Add style classes to DirectoryEntry.js

--- src/components/organisms/DirectoryEntry/DirectoryEntry.js
+++ src/components/organisms/DirectoryEntry/DirectoryEntry.js
@@ -1,28 +1,48 @@
 import React from 'react';
 import Button from 'react-toolbox/lib/button/Button';
+import FontIcon from 'react-toolbox/lib/font_icon/FontIcon';
 
 class DirectoryEntry extends React.Component {
   render() {
     return (
-      <div>
-        <div className="row">
-          <div className="col-xs-6">
-            <p>NY Strong</p>
-            <p>300 Phillips Park Rd.</p>
-            <p>2nd Floor</p>
-            <p>Mamaroneck, NY 10543</p>
+      <div className="directory-entry">
+        <div className="row bottom-xs">
+          <div className="col-xs-6
+                          directory-entry__location-contact-info">
+            <h3 className="directory-entry__location-name">
+              NY Strong
+            </h3>
+            <p className="directory-entry__location-line">
+              300 Phillips Park Rd.
+            </p>
+            <p className="directory-entry__location-line">
+              2nd Floor
+            </p>
+            <p className="directory-entry__location-line">
+              Mamaroneck, NY 10543
+            </p>
           </div>
-          <div className="col-xs-6">
-            <p>Adult Open Gym</p>
-            <p>Adult Classes</p>
+          <div className="col-xs-6 directory-entry__location-other-info">
+            <p className="directory-entry__other-line">
+              <FontIcon value='check_circle' className="directory-entry__icon directory-entry__icon--pass"/>
+              Adult Open Gym
+            </p>
+            <p className="directory-entry__other-line">
+              <FontIcon value='cancel' className="directory-entry__icon directory-entry__icon--fail"/>
+              Adult Classes
+            </p>
           </div>
         </div>
-        <div className="row">
-          <div className="col-xs-6">
-            <Button icon='phone' raised primary>Call</Button>
+        <div className="row directory-entry__cta-buttons">
+          <div className="col-xs-6 end-xs col-sm start-sm">
+            <div className="box hidden-sm">
+              <Button icon='phone' raised primary>Call</Button>
+            </div>
           </div>
-          <div className="col-xs-6">
-            <Button icon='near_me' raised>Map</Button>
+          <div className="col-xs-6 start-xs">
+            <div className="box hidden-sm">
+              <Button icon='near_me' raised>Map</Button>
+            </div>
           </div>
         </div>
         <hr/>
