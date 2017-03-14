77fbd1afd91ee1232c38b108d34a0214b1934298
Chris Salvato <csalvato@gmail.com>
2017-03-14 09:47:38 -0400

    Finish styling of phone and map buttons/links.

--- src/components/organisms/DirectoryEntry/DirectoryEntry.js
+++ src/components/organisms/DirectoryEntry/DirectoryEntry.js
@@ -21,6 +21,12 @@ class DirectoryEntry extends React.Component {
             <p className="directory-entry__location-line">
               Mamaroneck, NY 10543
             </p>
+            <p className="directory-entry__location-line hidden-xs">
+              <a href="#">
+                <FontIcon value='phone' className="directory-entry__icon"/>
+                (718)-252-3974
+              </a>
+            </p>
           </div>
           <div className="col-xs-6 directory-entry__location-other-info">
             <p className="directory-entry__other-line">
@@ -31,16 +37,22 @@ class DirectoryEntry extends React.Component {
               <FontIcon value='cancel' className="directory-entry__icon directory-entry__icon--fail"/>
               Adult Classes
             </p>
+            <p className="directory-entry__other-line hidden-xs">
+              <a href="#">
+                <FontIcon value='near_me' className="directory-entry__icon"/>
+                See on Google Maps
+              </a>
+            </p>
           </div>
         </div>
-        <div className="row directory-entry__cta-buttons">
+        <div className="row directory-entry__cta-buttons visible-xs">
           <div className="col-xs-6 end-xs col-sm start-sm">
-            <div className="box hidden-sm">
+            <div className="box">
               <Button icon='phone' raised primary>Call</Button>
             </div>
           </div>
           <div className="col-xs-6 start-xs">
-            <div className="box hidden-sm">
+            <div className="box">
               <Button icon='near_me' raised>Map</Button>
             </div>
           </div>
