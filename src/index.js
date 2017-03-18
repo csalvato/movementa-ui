bf3106f862a057c53ec4472000751b335de628a3
Chris Salvato <csalvato@gmail.com>
2017-03-18 08:17:55 -0400

    Get autocomplete first pass working.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
 import './index.css';
-import theme from './toolbox/theme.js'
+import theme from './toolbox/theme.js';
 import './toolbox/theme.css';
 import 'flexboxgrid';
-import 'flexboxgrid-helpers/dist/flexboxgrid-helpers.min.css'
+import 'flexboxgrid-helpers/dist/flexboxgrid-helpers.min.css';
 import 'roboto-fontface';
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
 import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
-import { createHistory } from 'history'
-import { Router, useRouterHistory } from 'react-router'
+import { createHistory } from 'history';
+import { Router, useRouterHistory } from 'react-router';
 
-import routes from 'routes'
+import routes from 'routes';
 
 const root = document.getElementById('root')
 const history = useRouterHistory(createHistory)({ basename: process.env.PUBLIC_PATH })
