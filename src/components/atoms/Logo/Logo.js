4013fdde33a1358fce3061e46cec85be3560ca58
Chris Salvato <csalvato@gmail.com>
2017-03-20 23:55:44 -0400

    Change Logo to allow a version with and without text.

--- src/components/atoms/Logo/Logo.js
+++ src/components/atoms/Logo/Logo.js
@@ -2,11 +2,16 @@ import React from 'react';
 import logo from './logo.png';
 
 class Logo extends React.Component {
+  renderText() {
+    return (<span className="logo__text--large">MOVEMENTA</span>);
+  }
+
   render() {
+    const logoText = this.props.withText ? this.renderText() : null;
     return (
       <div className="logo box">
         <img src={logo} className="logo__image--large box" alt="logo" />
-        <span className="logo__text--large">MOVEMENTA</span>
+        {logoText}
       </div>
     );
   }
