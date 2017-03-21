08518c52d50a731f83b6fe4d9b2f00a9b5b41788
Chris Salvato <csalvato@gmail.com>
2017-03-21 00:13:26 -0400

    Fix logo text overlapping with search form.

--- src/components/molecules/AppBarHeader/AppBarHeader.js
+++ src/components/molecules/AppBarHeader/AppBarHeader.js
@@ -12,10 +12,10 @@ class AppBarHeader extends React.Component {
               <div className="col-xs-1 visible-xs">
                 <Logo/>
               </div>
-              <div className="col-sm-2 hidden-xs">
+              <div className="col-sm-3 hidden-xs">
                 <Logo withText/>
               </div>
-              <div className="col-xs-10 col-sm-9">
+              <div className="col-xs-11 col-sm-9">
                 <SearchForm horizontal/>
               </div>
             </AppBar>
