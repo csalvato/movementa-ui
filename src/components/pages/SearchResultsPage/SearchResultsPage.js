35c33b0805d355a35b8b465128cc7f60df234dce
Chris Salvato <csalvato@gmail.com>
2017-04-01 14:24:08 -0400

    Update story for search results page

--- src/components/pages/SearchResultsPage/SearchResultsPage.js
+++ src/components/pages/SearchResultsPage/SearchResultsPage.js
@@ -17,12 +17,62 @@ class SearchResultsPage extends React.Component {
           <div className="row">
             <div className="col-xs-12 col-sm-8 col-sm-offset-2 search-results-page__entries">
               <div className="box">
-                <DirectoryEntry/>
-                <DirectoryEntry/>
-                <DirectoryEntry/>
-                <DirectoryEntry/>
-                <DirectoryEntry/>
-                <DirectoryEntry/>
+              <DirectoryEntry
+                location={ { name: "NY Strong",
+                             addressLine1: "300 Phillips Park Rd.",
+                             addressLine2: "Suite 205",
+                             city: "Mamaroneck",
+                             state: "NY",
+                             zip: "10543",
+                             phone: "718-252-3974",
+                             email: "csalvato@gmail.com",
+                             lat: -78.2490104891,
+                             lng: -73.2490104891,
+                             hasAdultOpenGym: true,
+                             hasAdultGymnasticsClasses: false
+                           } }/>
+             <DirectoryEntry
+               location={ { name: "NY Strong",
+                            addressLine1: "300 Phillips Park Rd.",
+                            addressLine2: "Suite 205",
+                            city: "Mamaroneck",
+                            state: "NY",
+                            zip: "10543",
+                            phone: "718-252-3974",
+                            email: "csalvato@gmail.com",
+                            lat: -78.2490104891,
+                            lng: -73.2490104891,
+                            hasAdultOpenGym: true,
+                            hasAdultGymnasticsClasses: false
+                          } }/>
+              <DirectoryEntry
+                location={ { name: "NY Strong",
+                             addressLine1: "300 Phillips Park Rd.",
+                             addressLine2: "Suite 205",
+                             city: "Mamaroneck",
+                             state: "NY",
+                             zip: "10543",
+                             phone: "718-252-3974",
+                             email: "csalvato@gmail.com",
+                             lat: -78.2490104891,
+                             lng: -73.2490104891,
+                             hasAdultOpenGym: true,
+                             hasAdultGymnasticsClasses: false
+                           } }/>
+             <DirectoryEntry
+               location={ { name: "NY Strong",
+                            addressLine1: "300 Phillips Park Rd.",
+                            addressLine2: "Suite 205",
+                            city: "Mamaroneck",
+                            state: "NY",
+                            zip: "10543",
+                            phone: "718-252-3974",
+                            email: "csalvato@gmail.com",
+                            lat: -78.2490104891,
+                            lng: -73.2490104891,
+                            hasAdultOpenGym: true,
+                            hasAdultGymnasticsClasses: false
+                          } }/>
               </div>
             </div>
           </div>
