6218df42957a6f2adacc248fcf3cf0b075c4573f
Chris Salvato <csalvato@gmail.com>
2017-04-02 18:35:41 -0400

    Fix error with PropType.

--- src/components/atoms/Logo/Logo.js
+++ src/components/atoms/Logo/Logo.js
@@ -2,7 +2,7 @@ import React, { PropTypes } from 'react';
 import logo from './logo.png';
 
 const propTypes = {
-  withText: PropTypes.boolean
+  withText: PropTypes.bool
 };
 
 const defaultProps = {
