24caa91fcecf2c73089d15aa7aeee5f5b9a20746
Chris Salvato <csalvato@gmail.com>
2017-04-01 22:59:17 -0400

    DirectoryEntry to stateless function

--- src/components/organisms/DirectoryEntry/DirectoryEntry.js
+++ src/components/organisms/DirectoryEntry/DirectoryEntry.js
@@ -25,96 +25,88 @@ const defaultProps = {
 
 };
 
-class DirectoryEntry extends React.Component {
-  get googleMapsUrl() {
-    const location = this.props.location
-    var full_address = `${location.addressLine1} ${location.addressLine2} ${location.city} ${location.state} ${location.zip}`
-    full_address = full_address.replace(/ /g, "+");
-    return `http://maps.google.com/?q=${full_address}`
-  }
+function DirectoryEntry({ location }) {
+  const full_address = [location.addressLine1,
+                        location.addressLine2,
+                        location.city,
+                        location.state,
+                        location.zip].join(" ").replace(/ /g, "+");
+  const googleMapsUrl = `http://maps.google.com/?q=${full_address}`
+  const phoneString = location.phone.replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3');
+  const phoneUrl = `tel:${location.phone}`
 
-  get phoneString() {
-    return this.props.location.phone.replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3');
-  }
-
-  get phoneUrl() {
-    return `tel:${this.props.location.phone}`
-  }
-
-  render() {
-    return (
-      <div className="directory-entry">
-        <div className="row bottom-xs">
-          <div className="col-xs-6
-                          directory-entry__location-contact-info">
-            <h3 className="directory-entry__location-name">
-              { this.props.location.name }
-            </h3>
-            <p className="directory-entry__location-line">
-              { this.props.location.addressLine1 }
-            </p>
-            <p className="directory-entry__location-line">
-              { this.props.location.addressLine2 }
-            </p>
-            <p className="directory-entry__location-line">
-              { this.props.location.city }, { this.props.location.state } { this.props.location.zip }
-            </p>
-            <p className="directory-entry__location-line hidden-xs">
-              <a href={this.phoneUrl}>
-                <FontIcon value='phone' className="directory-entry__icon"/>
-                { this.phoneString }
-              </a>
-            </p>
-          </div>
-          <div className="col-xs-6 directory-entry__location-other-info">
-            <p className="directory-entry__other-line">
-              <FeatureIcon hasFeature={ this.props.location.hasAdultOpenGym }>
-                Adult Open Gym
-              </FeatureIcon>
-            </p>
-            <p className="directory-entry__other-line">
-              <FeatureIcon hasFeature={ this.props.location.hasAdultGymnasticsClasses } >
-                Adult Classes
-              </FeatureIcon>
-            </p>
-            <p className="directory-entry__other-line hidden-xs">
-              <a href={ this.googleMapsUrl } target='_blank'>
-                <FontIcon value='near_me' className="directory-entry__icon"/>
-                See on Google Maps
-              </a>
-            </p>
-          </div>
+  return (
+    <div className="directory-entry">
+      <div className="row bottom-xs">
+        <div className="col-xs-6
+                        directory-entry__location-contact-info">
+          <h3 className="directory-entry__location-name">
+            { location.name }
+          </h3>
+          <p className="directory-entry__location-line">
+            { location.addressLine1 }
+          </p>
+          <p className="directory-entry__location-line">
+            { location.addressLine2 }
+          </p>
+          <p className="directory-entry__location-line">
+            { location.city }, { location.state } { location.zip }
+          </p>
+          <p className="directory-entry__location-line hidden-xs">
+            <a href={phoneUrl}>
+              <FontIcon value='phone' className="directory-entry__icon"/>
+              { phoneString }
+            </a>
+          </p>
         </div>
-        <div className="row directory-entry__cta-buttons visible-xs">
-          <div className="col-xs-6 end-xs col-sm start-md">
-            <div className="box">
-              <Button
-                raised
-                primary
-                icon='phone'
-                href={this.phoneUrl}
-              >
-                Call
-              </Button>
-            </div>
-          </div>
-          <div className="col-xs-6 start-xs">
-            <div className="box">
-              <Button
-                raised
-                icon='near_me'
-                href={ this.googleMapsUrl }
-                target='_blank'
-              >
-                Map
-              </Button>
-            </div>
-          </div>
+        <div className="col-xs-6 directory-entry__location-other-info">
+          <p className="directory-entry__other-line">
+            <FeatureIcon hasFeature={ location.hasAdultOpenGym }>
+              Adult Open Gym
+            </FeatureIcon>
+          </p>
+          <p className="directory-entry__other-line">
+            <FeatureIcon hasFeature={ location.hasAdultGymnasticsClasses } >
+              Adult Classes
+            </FeatureIcon>
+          </p>
+          <p className="directory-entry__other-line hidden-xs">
+            <a href={ googleMapsUrl } target='_blank'>
+              <FontIcon value='near_me' className="directory-entry__icon"/>
+              See on Google Maps
+            </a>
+          </p>
         </div>
-        <hr/>
       </div>
-    );
-  }
+      <div className="row directory-entry__cta-buttons visible-xs">
+        <div className="col-xs-6 end-xs col-sm start-md">
+          <div className="box">
+            <Button
+              raised
+              primary
+              icon='phone'
+              href={phoneUrl}
+            >
+              Call
+            </Button>
+          </div>
+        </div>
+        <div className="col-xs-6 start-xs">
+          <div className="box">
+            <Button
+              raised
+              icon='near_me'
+              href={ googleMapsUrl }
+              target='_blank'
+            >
+              Map
+            </Button>
+          </div>
+        </div>
+      </div>
+      <hr/>
+    </div>
+  );
 }
 
 DirectoryEntry.propTypes = propTypes;
