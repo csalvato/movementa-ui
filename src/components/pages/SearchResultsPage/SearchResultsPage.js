4b0a42fbd667f80a1095d8b137d26256a09e94c5
Chris Salvato <csalvato@gmail.com>
2017-04-02 23:01:06 -0400

    Turn AppBarHeader and SearchResultsPage to stateless functions.

--- src/components/pages/SearchResultsPage/SearchResultsPage.js
+++ src/components/pages/SearchResultsPage/SearchResultsPage.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import Layout from 'react-toolbox/lib/layout/Layout';
 import { DirectoryEntry, HeadlineHeader } from 'components';
 
-function SearchResultsPage() {
+const propTypes = {
+  results: PropTypes.arrayOf(<DirectoryEntry/>).isRequired
+};
+
+const defaultProps = {
+  results: []
+};
+
+function SearchResultsPage({ results }) {
   return (
     <div className="search-results-page">
       <div className="row">
@@ -16,62 +24,7 @@ function SearchResultsPage() {
         <div className="row">
           <div className="col-xs-12 col-sm-8 col-sm-offset-2 search-results-page__entries">
             <div className="box">
-            <DirectoryEntry
-              location={ { name: "NY Strong",
-                           addressLine1: "300 Phillips Park Rd.",
-                           addressLine2: "Suite 205",
-                           city: "Mamaroneck",
-                           state: "NY",
-                           zip: "10543",
-                           phone: "7182523974",
-                           email: "csalvato@gmail.com",
-                           lat: -78.2490104891,
-                           lng: -73.2490104891,
-                           hasAdultOpenGym: true,
-                           hasAdultGymnasticsClasses: false
-                         } }/>
-           <DirectoryEntry
-             location={ { name: "NY Strong",
-                          addressLine1: "300 Phillips Park Rd.",
-                          addressLine2: "Suite 205",
-                          city: "Mamaroneck",
-                          state: "NY",
-                          zip: "10543",
-                          phone: "7182523974",
-                          email: "csalvato@gmail.com",
-                          lat: -78.2490104891,
-                          lng: -73.2490104891,
-                          hasAdultOpenGym: true,
-                          hasAdultGymnasticsClasses: false
-                        } }/>
-            <DirectoryEntry
-              location={ { name: "NY Strong",
-                           addressLine1: "300 Phillips Park Rd.",
-                           addressLine2: "Suite 205",
-                           city: "Mamaroneck",
-                           state: "NY",
-                           zip: "10543",
-                           phone: "7182523974",
-                           email: "csalvato@gmail.com",
-                           lat: -78.2490104891,
-                           lng: -73.2490104891,
-                           hasAdultOpenGym: true,
-                           hasAdultGymnasticsClasses: false
-                         } }/>
-           <DirectoryEntry
-             location={ { name: "NY Strong",
-                          addressLine1: "300 Phillips Park Rd.",
-                          addressLine2: "Suite 205",
-                          city: "Mamaroneck",
-                          state: "NY",
-                          zip: "10543",
-                          phone: "7182523974",
-                          email: "csalvato@gmail.com",
-                          lat: -78.2490104891,
-                          lng: -73.2490104891,
-                          hasAdultOpenGym: true,
-                          hasAdultGymnasticsClasses: false
-                        } }/>
+              {results}
             </div>
           </div>
         </div>
--- src/components/pages/SearchResultsPage/SearchResultsPage.stories.js
+++ src/components/pages/SearchResultsPage/SearchResultsPage.stories.js
@@ -1,14 +1,71 @@
 import React from 'react'
 import { storiesOf } from '@kadira/storybook'
-import { SearchResultsPage } from 'components'
+import { SearchResultsPage, DirectoryEntry } from 'components'
 import AppTemplate from '../../templates/AppTemplate/AppTemplate'
+
+const results = [<DirectoryEntry
+                  location={ { name: "NY Strong",
+                               addressLine1: "300 Phillips Park Rd.",
+                               addressLine2: "Suite 205",
+                               city: "Mamaroneck",
+                               state: "NY",
+                               zip: "10543",
+                               phone: "7182523974",
+                               email: "csalvato@gmail.com",
+                               lat: -78.2490104891,
+                               lng: -73.2490104891,
+                               hasAdultOpenGym: true,
+                               hasAdultGymnasticsClasses: false
+                             } }/>,
+                <DirectoryEntry
+                 location={ { name: "NY Strong",
+                              addressLine1: "300 Phillips Park Rd.",
+                              addressLine2: "Suite 205",
+                              city: "Mamaroneck",
+                              state: "NY",
+                              zip: "10543",
+                              phone: "7182523974",
+                              email: "csalvato@gmail.com",
+                              lat: -78.2490104891,
+                              lng: -73.2490104891,
+                              hasAdultOpenGym: true,
+                              hasAdultGymnasticsClasses: false
+                            } }/>,
+                <DirectoryEntry
+                  location={ { name: "NY Strong",
+                               addressLine1: "300 Phillips Park Rd.",
+                               addressLine2: "Suite 205",
+                               city: "Mamaroneck",
+                               state: "NY",
+                               zip: "10543",
+                               phone: "7182523974",
+                               email: "csalvato@gmail.com",
+                               lat: -78.2490104891,
+                               lng: -73.2490104891,
+                               hasAdultOpenGym: true,
+                               hasAdultGymnasticsClasses: false
+                             } }/>,
+                <DirectoryEntry
+                 location={ { name: "NY Strong",
+                              addressLine1: "300 Phillips Park Rd.",
+                              addressLine2: "Suite 205",
+                              city: "Mamaroneck",
+                              state: "NY",
+                              zip: "10543",
+                              phone: "7182523974",
+                              email: "csalvato@gmail.com",
+                              lat: -78.2490104891,
+                              lng: -73.2490104891,
+                              hasAdultOpenGym: true,
+                              hasAdultGymnasticsClasses: false
+                            } }/>]
 
 storiesOf('SearchResultsPage', module)
   .add('embedded', () => (
     <AppTemplate>
-      <SearchResultsPage />
+      <SearchResultsPage results={results}/>
     </AppTemplate>
   ))
   .add('standalone', () => (
-      <SearchResultsPage />
+      <SearchResultsPage results={results}/>
     ))
