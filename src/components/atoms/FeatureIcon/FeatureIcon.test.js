f0b39201bb393194b6f3f3092be646653428cd75
Chris Salvato <csalvato@gmail.com>
2017-04-09 12:13:05 -0400

    Write tests for FeatureIcon.

--- src/components/atoms/FeatureIcon/FeatureIcon.test.js
+++ src/components/atoms/FeatureIcon/FeatureIcon.test.js
@@ -1,7 +1,40 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 import FeatureIcon from './FeatureIcon'
+import FontIcon from 'react-toolbox/lib/font_icon/FontIcon';
 
-it('renders', () => {
+it('renders without crashing with children', () => {
   shallow(<FeatureIcon>test</FeatureIcon>)
+})
+
+it('throws an error when rendered without children', () => {
+  // Spy on console.error
+  console.error = jest.fn()
+  shallow(<FeatureIcon/>)
+  expect(console.error).toHaveBeenCalled();
+})
+
+it('renders with child text', () => {
+  const wrapper = shallow(<FeatureIcon>test</FeatureIcon>)
+  expect(wrapper.contains("test")).toEqual(true)
+})
+
+it('renders with FontIcon with value of check_circle when hasFeature is set', () => {
+  const wrapper = mount(<FeatureIcon hasFeature>test</FeatureIcon>)
+  expect(wrapper.find(FontIcon).prop('value')).toEqual('check_circle')
+})
+
+it('renders with FontIcon with value of cancel when hasFeature is not set', () => {
+  const wrapper = mount(<FeatureIcon>test</FeatureIcon>)
+  expect(wrapper.find(FontIcon).prop('value')).toEqual('cancel')
+})
+
+it('renders with FontIcon class feature-icon--fail when hasFeature is set', () => {
+  const wrapper = mount(<FeatureIcon hasFeature>test</FeatureIcon>)
+  expect(wrapper.find(FontIcon).hasClass('feature-icon--pass')).toEqual(true)
+})
+
+it('renders with FontIcon class feature-icon--pass when hasFeature is not set', () => {
+  const wrapper = mount(<FeatureIcon>test</FeatureIcon>)
+  expect(wrapper.find(FontIcon).hasClass('feature-icon--fail')).toEqual(true)
 })
