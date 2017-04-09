8ba9c5092e5b30feb451f25ceac8c6ecf94e8664
Chris Salvato <csalvato@gmail.com>
2017-04-08 20:18:50 -0400

    Fix broken spec

--- src/components/atoms/FeatureIcon/FeatureIcon.test.js
+++ src/components/atoms/FeatureIcon/FeatureIcon.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import { FeatureIcon } from './FeatureIcon'
+import FeatureIcon from './FeatureIcon'
 
 it('renders', () => {
   shallow(<FeatureIcon>test</FeatureIcon>)
