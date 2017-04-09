23e0ef0ff0e3fd9bf2956859647c20532d51af77
Chris Salvato <csalvato@gmail.com>
2017-04-09 12:27:53 -0400

    Write Logo component tests

--- src/components/atoms/Logo/Logo.test.js
+++ src/components/atoms/Logo/Logo.test.js
@@ -5,3 +5,13 @@ import Logo from './Logo'
 it('renders', () => {
   shallow(<Logo />)
 })
+
+it('renders without text by default', () => {
+  const wrapper = shallow(<Logo/>)
+  expect(wrapper.contains('MOVEMENTA')).toEqual(false)
+})
+
+it('renders', () => {
+  const wrapper = shallow(<Logo withText/>)
+  expect(wrapper.contains('MOVEMENTA')).toEqual(true)
+})
