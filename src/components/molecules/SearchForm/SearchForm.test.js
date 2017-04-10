8071bb56719f7cc5a971b395d95b539f502db73e
Chris Salvato <csalvato@gmail.com>
2017-04-09 20:06:58 -0400

    Add some tests to SearchForm.

--- src/components/molecules/SearchForm/SearchForm.test.js
+++ src/components/molecules/SearchForm/SearchForm.test.js
@@ -2,6 +2,16 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import ConnectedSearchForm, { SearchForm } from './SearchForm'
 
-it('renders', () => {
+it('renders without crashing', () => {
   shallow(<SearchForm />)
 })
+
+it("renders with two rows by default", function(){
+  const wrapper = shallow(<SearchForm />);
+  expect(wrapper.find('.row')).toHaveLength(2)
+});
+
+it("renders with one rows when horizontal specified", function(){
+  const wrapper = shallow(<SearchForm horizontal/>);
+  expect(wrapper.find('.row')).toHaveLength(1)
+});
