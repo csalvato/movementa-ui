28ca5bbe7fa838b9fce33d37cc83367804dedca2
Chris Salvato <csalvato@gmail.com>
2017-04-09 22:35:27 -0400

    Add HomeTemplate tests

--- src/components/templates/HomeTemplate/HomeTemplate.test.js
+++ src/components/templates/HomeTemplate/HomeTemplate.test.js
@@ -5,3 +5,12 @@ import HomeTemplate from './HomeTemplate'
 it('renders', () => {
   shallow(<HomeTemplate />)
 })
+
+it('renders with children', () => {
+  shallow(<HomeTemplate><div>test</div></HomeTemplate>)
+})
+
+it('renders with children content', () => {
+  const wrapper = shallow(<HomeTemplate><div>foo</div></HomeTemplate>)
+  expect(wrapper).toIncludeText("foo")
+})
