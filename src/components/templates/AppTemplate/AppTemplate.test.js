af6ddd716932c01d8e70db20e1eda386eae43f55
Chris Salvato <csalvato@gmail.com>
2017-04-09 22:34:23 -0400

    Add AppTemplate tests

--- src/components/templates/AppTemplate/AppTemplate.test.js
+++ src/components/templates/AppTemplate/AppTemplate.test.js
@@ -5,3 +5,12 @@ import AppTemplate from './AppTemplate'
 it('renders', () => {
   shallow(<AppTemplate />)
 })
+
+it('renders with children', () => {
+  shallow(<AppTemplate><div>test</div></AppTemplate>)
+})
+
+it('renders with children content', () => {
+  const wrapper = shallow(<AppTemplate><div>foo</div></AppTemplate>)
+  expect(wrapper).toIncludeText("foo")
+})
