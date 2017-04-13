395866478b28e8ac0ad76b9ea25015654852b516
Chris Salvato <csalvato@gmail.com>
2017-04-12 23:21:03 -0400

    Delete routes and update index.js to use React Router Redux.

--- src/index.js
+++ src/index.js
@@ -8,38 +8,45 @@ import 'flexboxgrid-helpers/dist/flexboxgrid-helpers.min.css';
 import 'roboto-fontface';
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
 import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
-import { createHistory } from 'history';
-import { Router, useRouterHistory } from 'react-router';
 import 'airbnb-js-shims';
 import { Provider } from 'react-redux'
-import { storeWithEmptyState } from 'configureStore'
+import { storeWithEmptyState, history } from 'configureStore'
+import { ConnectedRouter } from 'react-router-redux'
+import { Route } from 'react-router';
+import { HomePage,
+         SearchResultsPage } from 'components';
+import AppTemplate from './components/templates/AppTemplate/AppTemplate'
 
-import routes from 'routes';
-import ReactGA from 'react-ga';
+// Google Analytics code broke when moving to react-router-redux
+//  Must be reimplemented using Middleware.
 
-if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-  ReactGA.initialize('UA-96092057-2'); //Dev tracking code
-} else {
-  ReactGA.initialize('UA-96092057-1'); //Prod tracking code
-}
-
-function logPageView() {
-  ReactGA.set({ page: window.location.pathname });
-  ReactGA.pageview(window.location.pathname);
-}
+// import ReactGA from 'react-ga';
+// if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+//   ReactGA.initialize('UA-96092057-2'); //Dev tracking code
+// } else {
+//   ReactGA.initialize('UA-96092057-1'); //Prod tracking code
+// }
+//
+// function logPageView() {
+//   ReactGA.set({ page: window.location.pathname });
+//   ReactGA.pageview(window.location.pathname);
+// }
 
 const root = document.getElementById('root')
-const history = useRouterHistory(createHistory)({ basename: process.env.PUBLIC_PATH })
 
 const store = storeWithEmptyState;
 
 const renderApp = () => (
   <Provider store={store}>
     <ThemeProvider theme={theme}>
-      <Router key={Math.random()}
-              history={history}
-              routes={routes}
-              onUpdate={logPageView}/>
+      <ConnectedRouter history={history}>
+        <div>
+          <Route exact path="/" component={HomePage}/>
+          <Route path="/search" component={AppTemplate}/>
+          <Route path="/search" component={SearchResultsPage}/>
+          {/* <Route path="*" component={NotFoundPage} /> */}
+        </div>
+      </ConnectedRouter>
     </ThemeProvider>
   </Provider>
 )
